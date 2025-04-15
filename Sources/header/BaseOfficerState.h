6c1d2c6a9fd72ca4704582a30a8aa61630ddc6d6
TMIC-GAME3-008\student1 <mcd2376075@stu.o-hara.ac.jp>
2025-04-15 11:33:37 +0900

    OfficerManagerを追加

--- Sources/header/BaseOfficerState.h
+++ Sources/header/BaseOfficerState.h
@@ -1,10 +1,17 @@
 #pragma once
 
+// ‘O•ûéŒ¾
+class Officer;
+
 /*
  * Sein
  * Officer‚ÌState‚ÌŠî’êƒNƒ‰ƒX
  */
 class BaseOfficerState
 {
+public:
+	virtual void Updata(Officer* officer){}
+	virtual void Enter(Officer* officer){}
+	virtual void Exit(Officer* officer){}
 
 };
