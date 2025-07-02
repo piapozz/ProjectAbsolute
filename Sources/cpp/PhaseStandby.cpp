0c2932d94c7bf3d93df9ebcefaff63f8808cb60a
Ishihara <mcd2376050@stu.o-hara.ac.jp>
2025-07-02 11:18:14 +0900

    フィルター更新

--- Sources/cpp/PhaseStandby.cpp
+++ Sources/cpp/PhaseStandby.cpp
@@ -1,12 +1,15 @@
 #include "../header/PhaseStandby.h"
+#include "../header/UIOfficer.h"
+#include "../header/ObjectFactory.h"
 
 void PhaseStandby::Init()
 {
 	// 入力受付クラスを生成
-
 	// キャラの詳細UI(入力なし)
+	UIOfficer* uiOfficer = ObjectFactory::Instance().CreateWithArgs<UIOfficer>();
 	// 所持金UI(入力なし)
 	// 区画に生成するキャラクターUI
 	// 控えキャラクターUI
 	// 開始ボタン
+	UIScreenButton* startButton = ObjectFactory::Instance().CreateWithArgs<UIScreenButton>();
 }
